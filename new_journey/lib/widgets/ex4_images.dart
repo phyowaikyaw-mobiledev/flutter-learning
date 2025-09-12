67b48b613d00b02e17eed2826d3fdc329a0880b4
phyowaikyaw-flutterdev <andrewtechstudio@gmail.com>
2025-09-12 14:43:42 +0700

    positioning photo

--- new_journey/lib/widgets/ex4_images.dart
+++ new_journey/lib/widgets/ex4_images.dart
@@ -6,7 +6,23 @@ class Ex4Images extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
       return Scaffold(
+        body: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                image: DecorationImage(
+                    image:AssetImage('images/billie.jpg'),
+                  fit: BoxFit.fitWidth,
+                )
+              ),
+              width: 300,
+              height: 300,
+              child: Text('Billie Ellish',style: TextStyle(
+                color: Colors.white,
+                //child:Image.network('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.indiamart.com%2Fproddetail%2F3d-wallpaper-20773721373.html%3Fsrsltid%3DAfmBOoogAx9mROVB_XvTLL9dgyP3PphUgRi6PEbOofanZPrtrn8AAmsa&psig=AOvVaw3WqPlrf52TZg-9hUMOw7RX&ust=1757748476429000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIie7L7Z0o8DFQAAAAAdAAAAABAE')
 
+              ),textAlign: TextAlign.center,),
+            )),
       );
     }
   }
